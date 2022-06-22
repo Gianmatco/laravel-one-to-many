d20ace7541deadb7deb1a83d0066a65a24eff429
gianmarco <gianmarco.culqui.ikaros@gmail.com>
2022-06-22 15:37:21 +0200

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').options({
+        processCssUrls: false
+    }).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*','public/fonts/font-awesome');
+
+//FRONTOFFICE
+// mix.js('resources/js/front.js', 'public/js')
+//     .sass('resources/sass/front.scss', 'public/css').options({
+//         processCssUrls: false});
